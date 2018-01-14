8bf88d8777177223a409be26913736775dfe1e2a
jbmagination <jbm@pupcus.org>
2018-01-14 10:34:51 -0500

    replaced the html single quotes ' with the text single quotes '

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready to give @Goldminer127#6374 some cash!’);
+    console.log('I am ready to give @Goldminer127#6374 some cash!');
 });
 
 client.on('message', message => {
     if (message.content === 'That's a floating tree... Did you guys just turn off gravity or something?') {
-    	message.reply(‘Congratulations! @Goldminer127#6374 now has $2 more in his bank account.’);
+    	message.reply('Congratulations! @Goldminer127#6374 now has $2 more in his bank account.');
   	}
 });
 
